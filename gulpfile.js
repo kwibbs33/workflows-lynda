4af03243d4cf05d92f90ce1017d3fba673c9c38a
KW <kwibbs33@yahoo.com>
2016-06-20 16:12:06 -0700

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-	gutil.log('workflows yay');
+// coffeescript files set as an array - is easier to add multiples here than in the task
+var coffeeSources = ['components/coffee/tagline.coffee'];	// individual files or
+//var coffeeSources = ['components/coffee/*.coffee'];		// all files
+
+// compile coffeescript
+gulp.task('coffee', function() {
+	gulp.src(coffeeSources)	// get source files with gulp.src
+		.pipe(coffee({bare: true})					// options: bare - compile to js without safety wrapper
+			.on('error', gutil.log))				// log error so gulp doesn't crash on coffescript error
+		.pipe(gulp.dest('components/scripts'))		// output in new destination folder
 });
